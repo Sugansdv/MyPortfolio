4a18d6a50cab75a09ce12b2fea8fd71fc2f6e115
Sugansdv <suganyasdv16@gmail.com>
2025-05-23 08:31:25 +0530

    add

--- script/script.js
+++ script/script.js
@@ -7,27 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 
-     // Work filter
-     document.querySelectorAll('.work-item').forEach(item => item.classList.add('show'));
-    const filterItems = document.querySelectorAll('.filter-item');
-    const workItems = document.querySelectorAll('.work-item');
-    
-    filterItems.forEach(item => {
-        item.addEventListener('click', function() {
-            filterItems.forEach(filter => filter.classList.remove('active'));
-            this.classList.add('active');
-            
-            const filterValue = this.getAttribute('data-filter');
-            
-            workItems.forEach(item => {
-                if (filterValue === 'all' || item.classList.contains(filterValue)) {
-                    item.classList.add('show');
-                } else {
-                    item.classList.remove('show');
-                }
-            });
-        });
-    });
+     
 
     // Back to top button
     const backToTopButton = document.querySelector('.back-to-top');
